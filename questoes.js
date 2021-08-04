390842dbe97f42f652d0e9daf63957355769fd54
captainsisco <drg4176@gmail.com>
2021-08-04 20:50:17 -0300

    arquivo de questões transformado em js

---
+++ questoes.js
@@ -0,0 +1,18 @@
+﻿export const questoes = [
+  {
+    enunciado: `A rosa dos ventos é uma figura que representa oito sentidos, que dividem o círculo em partes iguais. Descrição da figura: Figura formada por dois círculos concêntricos. No maior, estão traçados dois diâmetros perpendiculares, um na vertical e outro na horizontal. Na extremidade superior do diâmetro vertical está a letra "N" (Norte) e na extremidade inferior, a letra "S" (Sul). Na extremidade à esquerda do diâmetro horizontal está a letra "O" (Oeste) e na extremidade à direita, a letra "L" (Leste). No círculo menor estão traçados dois diâmetros inclinados e perpendiculares. Na extremidade superior do diâmetro inclinados à esquerda estão as letras "NO" (Noroeste) e na extremidade inferior à direita estão as letras "SE" (Sudeste). No outro diâmetro inclinado há, na extremidade superior à direita, as letras "NE" (Nordeste) e na extremidade inferior à esquerda estão as letras "SO" (Sudoeste). Uma câmera de vigilância está fixada no teto de um shopping e sua lente pode ser direcionada remotamente, através de um controlador, para qualquer sentido. A lente da câmera está apontada inicialmente no sentido Oeste e o seu controlador efetua três mudanças consecutivas, a saber: Primeira mudança: 135 graus no sentido anti-horário; Segunda mudança: 60 graus no sentido horário; Terceira  mudança: 45 graus no sentido anti-horário. Após a terceira mudança, ele é orientado a reposicionar a câmera, com a menor amplitude possível, no sentido Noroeste (NO) devido a um movimento suspeito de um cliente. Qual mudança de sentido o controlador deve efetuar para reposicionar a câmera?`,
+    alternativas: [
+      '75 graus no sentido horário.',
+      '105 graus no sentido anti-horário.',
+      '120 graus no sentido anti-horário.',
+      '135 graus no sentido anti-horário.',
+      '165 graus no sentido horário.'
+    ],
+    correta: 4
+  }, {
+
+  },
+  {
+
+  }
+]
