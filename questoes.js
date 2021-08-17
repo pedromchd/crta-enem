1ba129507356759e0681313c9052da8e700db518
captainsisco <drg4176@gmail.com>
2021-08-17 20:33:31 -0300

    Adicionadas mais duas questões.

--- questoes.js
+++ questoes.js
@@ -1,18 +1,34 @@
-﻿export const questoes = [
-  {
-    enunciado: `A rosa dos ventos é uma figura que representa oito sentidos, que dividem o círculo em partes iguais. Descrição da figura: Figura formada por dois círculos concêntricos. No maior, estão traçados dois diâmetros perpendiculares, um na vertical e outro na horizontal. Na extremidade superior do diâmetro vertical está a letra "N" (Norte) e na extremidade inferior, a letra "S" (Sul). Na extremidade à esquerda do diâmetro horizontal está a letra "O" (Oeste) e na extremidade à direita, a letra "L" (Leste). No círculo menor estão traçados dois diâmetros inclinados e perpendiculares. Na extremidade superior do diâmetro inclinados à esquerda estão as letras "NO" (Noroeste) e na extremidade inferior à direita estão as letras "SE" (Sudeste). No outro diâmetro inclinado há, na extremidade superior à direita, as letras "NE" (Nordeste) e na extremidade inferior à esquerda estão as letras "SO" (Sudoeste). Uma câmera de vigilância está fixada no teto de um shopping e sua lente pode ser direcionada remotamente, através de um controlador, para qualquer sentido. A lente da câmera está apontada inicialmente no sentido Oeste e o seu controlador efetua três mudanças consecutivas, a saber: Primeira mudança: 135 graus no sentido anti-horário; Segunda mudança: 60 graus no sentido horário; Terceira  mudança: 45 graus no sentido anti-horário. Após a terceira mudança, ele é orientado a reposicionar a câmera, com a menor amplitude possível, no sentido Noroeste (NO) devido a um movimento suspeito de um cliente. Qual mudança de sentido o controlador deve efetuar para reposicionar a câmera?`,
-    alternativas: [
-      '75 graus no sentido horário.',
-      '105 graus no sentido anti-horário.',
-      '120 graus no sentido anti-horário.',
-      '135 graus no sentido anti-horário.',
-      '165 graus no sentido horário.'
-    ],
-    correta: 4
-  }, {
-
-  },
-  {
-
-  }
+﻿export const questoes= [
+{
+enunciado: 'A rosa dos ventos é uma figura que representa oito sentidos, que dividem o círculo em partes iguais. Descrição da figura: Figura formada por dois círculos concêntricos. No maior, estão traçados dois diâmetros perpendiculares, um na vertical e outro na horizontal. Na extremidade superior do diâmetro vertical está a letra "N" (Norte) e na extremidade inferior, a letra "S" (Sul). Na extremidade à esquerda do diâmetro horizontal está a letra "O" (Oeste) e na extremidade à direita, a letra "L" (Leste). No círculo menor estão traçados dois diâmetros inclinados e perpendiculares. Na extremidade superior do diâmetro inclinados à esquerda estão as letras "NO" (Noroeste) e na extremidade inferior à direita estão as letras "SE" (Sudeste). No outro diâmetro inclinado há, na extremidade superior à direita, as letras "NE" (Nordeste) e na extremidade inferior à esquerda estão as letras "SO" (Sudoeste). Uma câmera de vigilância está fixada no teto de um shopping e sua lente pode ser direcionada remotamente, através de um controlador, para qualquer sentido. A lente da câmera está apontada inicialmente no sentido Oeste e o seu controlador efetua três mudanças consecutivas, a saber: Primeira mudança: 135 graus no sentido anti-horário; Segunda mudança: 60 graus no sentido horário; Terceira  mudança: 45 graus no sentido anti-horário. Após a terceira mudança, ele é orientado a reposicionar a câmera, com a menor amplitude possível, no sentido Noroeste (NO) devido a um movimento suspeito de um cliente. Qual mudança de sentido o controlador deve efetuar para reposicionar a câmera?',
+alternativas: [
+'75 graus no sentido horário.',
+'105 graus no sentido anti-horário.',
+'120 graus no sentido anti-horário.',
+'135 graus no sentido anti-horário.',
+'165 graus no sentido horário.'
+],
+correta: 4
+}, {
+enunciado: 'A imagem apresentada na figura é uma cópia em preto e branco da tela quadrada intitulada O peixe, de Marcos Pinto, que foi colocada em uma parede para exposição e fixada nos pontos A e B. Por um problema na fixação de um dos pontos, a tela se desprendeu, girando rente à parede. Após o giro, ele ficou posicionada como ilustrado na figura, formando um ângulo de 45 graus com a linha do horizonte. Descrições das figuras: Figura 1: tela com forma de um quadrado, fixada pelos pontos A e B, sendo A o vértice superior esquerdo do quadrado, e B, o vértice inferior direito do quadrado; e o segmento da reta AB, a diagonal desse quadrado. Figura 2: a mesma tela fixada apenas pelo ponto B, do qual parte uma linha horizontal, formando, com um dos lados do quadrado, um ângulo de 45 graus. Para recolocar a tela na sua posição original, deve-se girá-la, rente à parede, no menor ângulo possível inferior a 360 graus. A forma de recolocar a tela a posição original, obedecendo ao que foi estabelecido, é girando-a em um ângulo de',
+alternativas: [
+'90 graus no sentido horário.',
+'135 graus no sentido horário.'
+'180 graus no sentido anti-horário.'
+'270 graus no sentido anti-horário.'
+'315 graus no sentido horário.'
+],
+correta: 1
+},
+{
+enunciado: 'Enem 2018 Os guindastes são fundamentais em canteiros de obras, no manejo de materiais pesados como vigas de aço. A figura ilustra uma sequência de estágios em que um guindaste iça uma viga de aço que se encontra inicialmente no solo. Descrição da figura: A figura representa três estágios do movimento de uma viga de aço por um guindaste: No estágio 1, a viga está sobre o chão. Há o ponto M que indica o ponto médio da viga. À direita da viga há o ponto O que indica a posição vertical do cabo de aço. No estágio 2, a viga está inclinada em relação ao chão, o lado esquerdo da viga está indo de encontro ao ponto O e o lado direito da viga subindo puxada pelo cabo de aço. No estágio 3, a viga está na posição vertical em relação ao chão com uma de suas extremidades apoiada no solo sobre o ponto O. Na figura, o ponto O representa a projeção ortogonal do cabo de aço sobre o plano do chão e este se mantém na vertical durante todo o movimento de içamento da viga, que se inicia no tempo igual a zero (estágio 1) e finaliza no tempo índice f (estágio 3). Uma das extremidades da viga é içada verticalmente a partir do ponto O, enquanto que a outra extremidade desliza sobre o solo em direção ao ponto O. Considere que o cabo de aço utilizado pelo guindaste para içar a viga fique sempre na posição vertical. Na figura, o ponto M representa o ponto médio do segmento que representa a viga. O gráfico que descreve a distância do ponto M ao ponto O, em função do tempo, entre tempo igual a zero e tempo índice f, é: ',
+alternativas: [
+'Descrição do gráfico: Os eixos cartesianos se interceptam na origem. O eixo horizontal representa o tempo e o eixo vertical, a distância. O gráfico é um segmento de reta paralelo ao eixo horizontal com uma das extremidades sobre o eixo vertical e a outra num ponto de abscissa tempo índice f.'
+'Descrição do gráfico: Os eixos cartesianos se interceptam na origem. O eixo horizontal representa o tempo e o eixo vertical, a distância. O gráfico é um segmento de reta inclinados para cima com uma das extremidades sobre o eixo vertical e a outra num ponto de abscissa tempo índice f.'
+'Descrição do gráfico: Os eixos cartesianos se interceptam na origem. O eixo horizontal representa o tempo e o eixo vertical, a distância. O gráfico é um segmento de reta inclinado para baixo com uma das extremidades sobre o eixo vertical e a outra num ponto de abscissa tempo índice f.'
+'Descrição do gráfico: Os eixos cartesianos se interceptam na origem. O eixo horizontal representa o tempo e o eixo vertical, a distância. O gráfico é uma curva com a concavidade voltada para baixo com uma das extremidades sobre o eixo vertical e a outra num ponto de abscissa tempo índice f.'
+'Descrição do gráfico: Os eixos cartesianos se interceptam na origem. O eixo horizontal representa o tempo e o eixo vertical, a distância. O gráfico é uma curva com a concavidade voltada para cima com umas das extremidades sobre o eixo vertical e a outra num ponto de abscissa tempo índice f.'
+],
+correta: 0
+}
 ]
